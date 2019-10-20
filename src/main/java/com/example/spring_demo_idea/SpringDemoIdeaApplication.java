7e95edd145fd59a8a5a1c956756ea182fce42327
Maxim Koveshnikov <max.kov@gmail.com>
2019-10-20 14:10:12 +0300

    Initial commit

---
+++ src/main/java/com/example/spring_demo_idea/SpringDemoIdeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_demo_idea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoIdeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoIdeaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_demo_idea/SpringDemoIdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_demo_idea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoIdeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
